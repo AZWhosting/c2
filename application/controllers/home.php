18204b37b3593ac471454dd2516da9dda8126b0b
Sokdararith Prak <praksokdararith@outlook.com>
2016-06-30 17:45:43 +0700

    another test

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,13 +1,13 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Home extends MY_Controller {
+class Home extends CI_Controller {
 	
 	function __construct() {
 		parent::__construct();
 	}
 	
 	public function index() {	
-		$this->_render("welcome_view");
+		$this->load->view("welcome_view");
 	}
 }
 
