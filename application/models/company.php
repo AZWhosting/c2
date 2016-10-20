762ec2177be6cb24dc6fdfa413870cdb466d48df
Nicaray <nicarayz@gmail.com>
2016-10-21 00:44:40 +0700

    fix company mission model

---
+++ application/models/company.php
@@ -0,0 +1,58 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Company extends DataMapper {
+	public $table = 'branches';
+	protected $created_field = 'created_at';
+	protected $updated_field = 'updated_at';
+	
+	public $has_one = array(		
+		'currency' => array(
+			"class" => 'currency',
+			"other_field" => "branch"
+		)
+	);
+
+	public $has_many = array('user',		
+		'fee' => array(
+			"class" => 'fee',
+			"other_field" => "branch"
+		),
+		'contact' => array(
+			"class" => 'contact',
+			"other_field" => "branch"
+		),
+		'location' => array(
+			"class" => 'location',
+			"other_field" => "branch"
+		),
+		'ebranch' => array(
+			"class" => 'contact',
+			"other_field" => "branch"
+		),
+		'wbranch' => array(
+			"class" => 'contact',
+			"other_field" => "branch"
+		)
+	);
+	
+	public function __construct($id = null, $server_name = null, $db_username = null, $server_password = null, $db = null) {	
+		$this->db_params = array(
+				'dbdriver' => 'mysql',
+				'pconnect' => true,
+				'db_debug' => true,
+				'cache_on' => false,
+				'char_set' => 'utf8',
+				'cachedir' => '',
+				'dbcollat' => 'utf8_general_ci',
+				'hostname' => 'banhji-db-instance.cwxbgxgq7thx.ap-southeast-1.rds.amazonaws.com',
+				'username' => 'mightyadmin',
+				'password' => 'banhji2016',
+				'database' => $db,
+				'prefix'   => ''
+			);
+		parent::__construct($id);
+	}
+}
+
+/* End of file branch.php */
+/* Location: ./application/models/branch.php */
