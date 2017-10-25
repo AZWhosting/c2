aefaf052f9dfed12acec7a476c7933a555311a9d
Nicaray <nicarayz@gmail.com>
2017-10-25 14:00:42 +0700

    edited createDB

--- application/controllers/api/inventory_modules.php
+++ application/controllers/api/inventory_modules.php
@@ -345,8 +345,8 @@ class Inventory_modules extends REST_Controller {
 				$itemLines = new Item_line(null, $this->server_host, $this->server_user, $this->server_pwd, $this->_database);
 				$itemLines->select_sum('quantity * conversion_ratio * movement', "totalQuantity");
 				$itemLines->select_sum('quantity * conversion_ratio * movement * cost', "totalAmount");
-				$itemLines->where_related("transaction", "is_recurring <>", 1);
 				$itemLines->where_related("transaction", "issued_date <", $asOf);
+				$itemLines->where_related("transaction", "is_recurring <>", 1);				
 				$itemLines->where_related("transaction", "deleted <>", 1);
 				$itemLines->where('item_id', $value->id);
 				$itemLines->where('movement <>', 0);
