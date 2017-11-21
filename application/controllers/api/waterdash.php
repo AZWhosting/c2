6c70e4a8a5bf5733f6e2fc913cdf3a019c8429f2
Nicaray <nicarayz@gmail.com>
2017-11-21 12:21:11 +0700

    kpi

--- application/controllers/api/waterdash.php
+++ application/controllers/api/waterdash.php
@@ -346,22 +346,22 @@ class Waterdash extends REST_Controller {
 				$totalAllowCustomer = $value->max_customer == 0 ? 0: ($nContact / intval($value->max_customer));
 
 		
-				// $trxSale = new Transaction(null, $this->server_host, $this->server_user, $this->server_pwd, $this->_database);
-				// $trxSale->select_sum('amount');
-				// $trxSale->where('type', 'Utility_Invoice');
-				// $trxSale->where('location_id', $loc->id)->get_iterated();
-				// $totalAmount += $trxSale->amount;
+				$trxSale = new Transaction(null, $this->server_host, $this->server_user, $this->server_pwd, $this->_database);
+				$trxSale->select_sum('amount');
+				$trxSale->where('type', 'Utility_Invoice');
+				$trxSale->where('location_id', $loc->id)->get_iterated();
+				$totalAmount += $trxSale->amount;
 
-				// $avgIncome = $activeCount == 0? 0 : $totalAmount  / $activeCount;
+				$avgIncome = $activeCount == 0? 0 : $totalAmount  / $activeCount;
 
-				// $avgUsage = new Meter_record(null, $this->server_host, $this->server_user, $this->server_pwd, $this->_database);
-				// $trxSale->where('location_id', $loc->id)->get_iterated();
-				// $avgUsage->get_iterated();
-				// $avg = 0;
-				// foreach($avgUsage as $avgUsg) {
-				// 	$totalUsage += $avgUsg->usage;
-				// }
-				// $avg = $activeCount == 0 ? 0:$totalUsage / $activeCount;
+				$avgUsage = new Meter_record(null, $this->server_host, $this->server_user, $this->server_pwd, $this->_database);
+				$trxSale->where('location_id', $loc->id)->get_iterated();
+				$avgUsage->get_iterated();
+				$avg = 0;
+				foreach($avgUsage as $avgUsg) {
+					$totalUsage += $avgUsg->usage;
+				}
+				$avg = $activeCount == 0 ? 0:$totalUsage / $activeCount;
 			}
 			$data['results'][] = array(
 				'id' => $value->id,
