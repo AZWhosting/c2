1f05723750640c4129147f65cd7155252d3979e7
Nicaray <nicarayz@gmail.com>
2018-01-04 14:38:08 +0700

    working on transfer movment

--- application/controllers/api/item_lines.php
+++ application/controllers/api/item_lines.php
@@ -235,6 +235,24 @@ class Item_lines extends REST_Controller {
 					$transaction->get_by_id($value->transaction_id);
 					
 					if($transaction->exists()){
+						
+						// if($transaction->reference_id>0){
+						// 	$referenceTxn = new Transaction(null, $this->server_host, $this->server_user, $this->server_pwd, $this->_database);
+						// 	$referenceTxn->get_by_id($transaction->reference_id);
+							
+						// 	if($transaction->type=="Cash_Purchase" || $transaction->type=="Credit_Purchase"){
+						// 		if($referenceTxn->type=="GRN"){
+						// 			$value->movement = 0;
+						// 		}
+						// 	}
+							
+						// 	if($transaction->type=="GRN"){
+						// 		if($referenceTxn->type=="Cash_Purchase" || $referenceTxn->type=="Credit_Purchase"){
+						// 			$value->movement = 0;
+						// 		}
+						// 	}
+						// }
+
 						if($value->movement==0){}else{
 							//Find Item Quantity and Amount
 							$itemLines = new Item_line(null, $this->server_host, $this->server_user, $this->server_pwd, $this->_database);							
